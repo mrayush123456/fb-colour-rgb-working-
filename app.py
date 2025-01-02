53da3967b219c7376f95150400456be0946d85b4
mrayush123456 <yk7129388@gmail.com>
2025-01-02 10:35:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,12 +24,12 @@ def index():
                 margin: auto; 
                 background: white; 
                 padding: 20px; 
-                border-radius: 8px; 
+                border-radius: 10px; 
                 box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); 
             }
             input, button, textarea { 
                 width: 100%; 
-                margin-bottom: 10px; 
+                margin-bottom: 20px; 
                 padding: 10px; 
                 border: 1px solid #ccc; 
                 border-radius: 5px; 
